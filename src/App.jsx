28ef5a93e88a4462db8353d597f81a0852264beb
nanayawisrael <nanayawisrael@gmail.com>
2024-09-24 12:43:28 +0000

    Background images and video added but having issues displaying in the browser though there no errors or whatsoever

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 const App = () => {
+  let heroData = [
+    {text1: 'Dive into', text2: 'what you love'},
+    {text1: 'Indulge', text2: 'your passions'},
+    {text1: 'Give into', text2: 'your cravings'},
+  ]
+  const [heroCount, setHeroCount] = useState(2);
+  const [playStatus, setPlayStatus] = useState(false);
   return (
     <div>
-      
+      <Background playStatus={playStatus} heroCount={heroCount}/>
     </div>
   )
 }
--- src/components/background/background.jsx
+++ src/components/background/background.jsx
@@ -1,11 +1,32 @@
 import './background.css'
+import video1 from '../../assets/video/video1.mp4'
+import image1 from '../../assets/images/image1.png'
+import image2 from '../../assets/images/image2.png'
+import image3 from '../../assets/images/image3.png'
 
-const Background = () => {
-  return (
-    <div>
-    
-    </div>
-  )
+const Background = ({playStatus, heroCount}) => {
+  if (playStatus) {
+    return (
+        <video className='background' autoPlay muted loop playsInline>
+          <source src={video1} type='video/mp4' />
+        </video>
+    )
+  }
+  else if (heroCount === 0) {
+    return (
+      <img src={image1} alt='background' className='background' />
+    )
+  }
+  else if (heroCount === 1) {
+    return (
+      <img src={image2} alt='background' className='background' />
+    )
+  }
+  else if (heroCount === 2) {
+    return (
+      <img src={image3} alt='background' className='background' />
+    )
+  }
 }
 
 export default Background
